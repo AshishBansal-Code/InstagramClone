4b01c635fc793fe25d5d22238ba89ec2e43fb6c4
ashishbansal-Code <ashishbansal350@gmail.com>
2025-04-20 11:26:42 +0530

    Login and Signup UI with view change

---
+++ app/src/main/java/com/example/instaclone/MainActivity.kt
@@ -0,0 +1,151 @@
+package com.example.instaclone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.instaclone.ui.theme.InstaCloneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            InstaCloneTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AuthScreen()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen(
+    onLoginClick: () ->Unit,
+    onSignUpClick:() -> Unit
+){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp)   ,
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center){
+        Text(text = "Instagram", fontSize = 36.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        var email by remember {
+            mutableStateOf("")
+        }
+        var password by remember {
+            mutableStateOf("")
+        }
+
+        OutlinedTextField(value = email, onValueChange = {email=it}, label = { Text(text = "Email")}, modifier = Modifier.fillMaxWidth())
+
+        Spacer(modifier = Modifier.height(36.dp))
+
+        OutlinedTextField(
+            value = password, onValueChange = {password=it},
+        label = { Text(text = "Password")},
+        modifier = Modifier.fillMaxWidth(),
+        visualTransformation = PasswordVisualTransformation())
+
+        Button(onClick = onLoginClick, modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Login")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextButton(onClick = onSignUpClick) {
+            Text(text = "Don't have an account? Sign Up")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SignUpScreen(
+    onSignUpClick: () -> Unit,
+    onLoginClick: () -> Unit
+){
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(32.dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        Text(text = "Instagram", fontSize = 36.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        var username by remember{ mutableStateOf("") }
+        var email by remember {
+            mutableStateOf("")
+        }
+        var password by remember {
+            mutableStateOf("")
+        }
+        OutlinedTextField(value = username, onValueChange = {username=it}, label = { Text(text = "Username")}, modifier = Modifier.fillMaxWidth())
+
+        Spacer(modifier = Modifier.height(16.dp))
+        
+        OutlinedTextField(value = email, onValueChange = {email = it}, label = { Text(text = "Email")}, modifier = Modifier.fillMaxWidth())
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(value = password, onValueChange = {password = it}, label = { Text(text = "Password")}, modifier = Modifier.fillMaxWidth(), visualTransformation = PasswordVisualTransformation())
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Button(onClick = onSignUpClick, modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Sign Up")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        
+        TextButton(onClick = onLoginClick) {
+            Text(text = "Already have an account? Log In")
+        }
+    }
+}
+
+@Composable
+fun AuthScreen(){
+    var showLogin by remember {
+        mutableStateOf(true)
+    }
+    if(showLogin){
+        LoginScreen(onLoginClick = { /*TODO*/ },
+        onSignUpClick = {showLogin = false})
+    }
+    else{
+        SignUpScreen(onSignUpClick = { /*TODO*/ }, onLoginClick = {showLogin=true})
+    }
+}
